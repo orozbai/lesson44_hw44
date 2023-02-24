cad92693dd29780f332c959b434ebf3afaf57933
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-02-24 18:51:55 +0600

    created Cookie, Lesson46Server, cookie.html

---
+++ src/kz/attractor/java/lesson44/Lesson46Server.java
@@ -0,0 +1,9 @@
+package kz.attractor.java.lesson44;
+
+import java.io.IOException;
+
+public class Lesson46Server extends Lesson45Server {
+    public Lesson46Server(String host, int port) throws IOException {
+        super(host, port);
+    }
+}
